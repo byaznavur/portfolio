91fc89de54a83746aa7040b11a634dec86cf9b66
azizdev28 <az1z.bakht1yarvich@gmail.com>
2024-11-26 00:49:47 +0500

    about page

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,9 +1,56 @@
 import { Fragment } from "react";
+import photoMe from "../../assets/about-image/image.png";
+import "./about.scss";
+import { FaGithub, FaLinkedin, FaTelegram, FaYoutube } from "react-icons/fa6";
 const About = () => {
   return (
     <div className="container about">
       <section className="aboutme">
-        <h2>About Me</h2>
+        <h2>Learn more about me</h2>
+        <div className="about-info">
+          <div className="about-image">
+            <img src={photoMe} alt="" />
+          </div>
+          <div className="about-text">
+            <h3>I'm Junior Front-End developer</h3>
+            <ul>
+              <li>
+                <span>Full Name</span>: Abulaziz Abduhakimov
+              </li>
+              <li>
+                <span>City</span>: Tashkent, Uzbekistan
+              </li>
+              <li>
+                <span>Birthday</span>: 28 October 2004
+              </li>
+              <li>
+                <span>Phone Number</span>: +998 99 688 2812
+              </li>
+              <li>
+                <span>Email</span>: az1z.bakht1yarvich@gmail.com
+              </li>
+              <li>
+                <span>Slogan</span>: Never give up
+              </li>
+            </ul>
+
+            <div className="about-socials">
+              <a href="">
+                <FaTelegram />
+              </a>
+              <a href="">
+                {" "}
+                <FaYoutube />
+              </a>
+              <a href="">
+                <FaLinkedin />
+              </a>
+              <a href="">
+                <FaGithub />
+              </a>
+            </div>
+          </div>
+        </div>
       </section>
       <section className="skills">
         <h2>Skills</h2>
