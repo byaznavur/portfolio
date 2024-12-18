3ce47b4707459a54b296f68644a7a9c958a3a2d4
azizdev28 <az1z.bakht1yarvich@gmail.com>
2024-12-18 18:35:48 +0500

    add new project page and projects

---
+++ src/data/project-data.js
@@ -0,0 +1,83 @@
+import img1 from "../assets/project-image/cleaning.png";
+import img2 from "../assets/project-image/lean.png";
+import img3 from "../assets/project-image/todo.png";
+import img4 from "../assets/project-image/avion.png";
+import img5 from "../assets/project-image/finsweet.png";
+import img6 from "../assets/project-image/gemini.png";
+import img7 from "../assets/project-image/noiceland.png";
+import img8 from "../assets/project-image/e-commerce.png";
+import img9 from "../assets/project-image/crud.png";
+export const projectData = [
+  {
+    id: 1,
+    img: img1,
+    title: "Cleaning-X",
+    link: "https://cleaning-x-five.vercel.app/",
+    github: "https://github.com/byaznavur/cleaning-x",
+    topics: ["CSS", "REACTJS", "RESPONSIVE", "STATIC"],
+  },
+  {
+    id: 2,
+    img: img2,
+    link: "https://lean-group-test1.vercel.app/",
+    title: "Lean-Group",
+    github: "https://github.com/byaznavur/Lean-Group",
+    topics: ["CSS", "REACTJS", "RESPONSIVE", "STATIC"],
+  },
+  {
+    id: 3,
+    img: img3,
+    link: "https://todo-react-murex-zeta.vercel.app/",
+    title: "Todo App",
+    github: "https://github.com/byaznavur/todo-app",
+    topics: ["CSS", "REACTJS", "RESPONSIVE", "DYNAMIC"],
+  },
+  {
+    id: 4,
+    img: img4,
+    link: "https://avion-clone.vercel.app/",
+    title: "Avion Clone",
+    github: "https://github.com/byaznavur/avion",
+    topics: ["CSS", "JAVASCRIPT", "RESPONSIVE", "DYNAMIC"],
+  },
+  {
+    id: 5,
+    img: img5,
+    link: "https://finsweetab.vercel.app/",
+    title: "FINSWEET",
+    github: "https://github.com/byaznavur/FINSWEET",
+    topics: ["CSS", "JAVASCRIPT", "RESPONSIVE", "DYNAMIC"],
+  },
+  {
+    id: 6,
+    img: img6,
+    link: "https://gemini-clone-chat-pink.vercel.app/",
+    title: "Gemini Clone",
+    github: "https://github.com/byaznavur/Chat-Bot",
+    topics: ["CSS", "JAVASCRIPT", "RESPONSIVE", "DYNAMIC"],
+  },
+  {
+    id: 7,
+    img: img7,
+    link: "https://test-first-project.vercel.app/",
+    title: "Gemini Clone",
+    github: "https://github.com/byaznavur/NOICELAND",
+    topics: ["CSS", "REACTJS", "RESPONSIVE", "DYNAMIC"],
+  },
+  {
+    id: 8,
+    img: img8,
+    link: "https://severyanochka-test-clone.vercel.app/",
+    title: "E-Commerce Severyanochka",
+    github: "https://github.com/byaznavur/SEVERYANOCHKA",
+    topics: ["HTML", "SCSS", "JAVASCRIPT", "RESPONSIVE", "DYNAMIC"],
+  },
+  {
+    id: 9,
+    img: img9,
+    link: "https://crud-app-rho-two.vercel.app/",
+    title: "CRUD APP",
+    github: "https://github.com/byaznavur/CRUD",
+    topics: ["HTML", "BOOTSTRAP", "JAVASCRIPT", "RESPONSIVE", "DYNAMIC"],
+  },
+];
--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,9 +1,33 @@
-import React from "react";
-
+import { projectData } from "../../data/project-data";
+import "./projects.scss";
+// import Projects from "./index";
 const Projects = () => {
   return (
-    <div>
-      <h1>Project</h1>
+    <div className="container project-wrapper">
+      <h2>My Projects</h2>
+
+      <div className="project-cards">
+        {projectData.map((el) => (
+          <div key={el.id} className="project-card">
+            <img src={el.img} alt="" />
+            <h3>
+              <span>{el.id}</span>.{el.title}
+            </h3>
+            <div className="project-link">
+              <a href={el.link}>Project Link</a>
+              <a href={el.github}>Git Hub </a>
+            </div>
+
+            <div className="topics">
+              {el.topics.map((topic) => (
+                <a key={topic} href="#">
+                  #{topic}
+                </a>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
